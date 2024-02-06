50f12520bb4f5140daca4bee361b62c6f4f8bd33
easydong19 <ymh1353@naver.com>
2024-02-06 23:55:32 +0900

    세가지 모델(nu-svr, lstm, garch) 모델링 완료

---
+++ common/calculate_volatility.py
@@ -0,0 +1,5 @@
+def calculate_variance(returns):
+    squared_log_price_return = (returns ** 2).dropna()
+    var = squared_log_price_return.rolling(window=5).sum() / 5.0
+
+    return var.dropna()
--- garch/garch_mle_example.py
+++ garch/garch_mle_example.py
@@ -1,16 +1,17 @@
 import numpy as np
-def garch_mle(params, returns,realized_vol):
+def garch_mle(params, returns):
     omega = params[0]
     alpha = params[1]
     beta = params[2]
 
+    long_run = (omega / (1 - alpha - beta)) ** (1 / 2)
     resid = returns
     realised = abs(resid)
 
     conditional = np.zeros(len(returns))
-    conditional[0] = realized_vol[0]
+    conditional[0] = long_run
     for t in range(1, len(returns)):
-        conditional[t] = np.sqrt(omega + alpha * resid[t - 1] ** 2 + beta * realized_vol[t - 1] ** 2)
+        conditional[t] = (omega + alpha * resid[t - 1] ** 2 + beta * conditional[t - 1] ** 2) ** (1 / 2)
 
     likelihood = 1/((2*np.pi)**(1/2)*conditional)*np.exp(-realised**2/(2*conditional**2))
     log_likelihood = np.sum(np.log(likelihood))
