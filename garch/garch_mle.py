6b81660b8a22adc5d56c6727d7b326fa5bd06afb
easydong19 <ymh1353@naver.com>
2024-02-15 15:51:25 +0900

    수정

--- garch/garch_mle.py
+++ garch/garch_mle.py
@@ -4,14 +4,14 @@ def garch_mle(params, returns,realized_vol):
     alpha = params[1]
     beta = params[2]
 
-    resid = returns
-    realised = abs(resid)
 
     conditional = np.zeros(len(returns))
     conditional[0] = realized_vol[0]
     for t in range(1, len(returns)):
-        conditional[t] = np.sqrt(omega + alpha * resid[t - 1] ** 2 + beta * realized_vol[t - 1] ** 2)
+        conditional[t] = np.sqrt(omega + alpha * returns[t - 1] ** 2 + beta * realized_vol[t - 1] ** 2)
 
-    likelihood = 1/((2*np.pi)**(1/2)*conditional)*np.exp(-realised**2/(2*conditional**2))
+    likelihood = 1/((2*np.pi)**(1/2)*conditional)*np.exp(-returns**2/(2*conditional**2))
     log_likelihood = np.sum(np.log(likelihood))
     return -log_likelihood
+
+
